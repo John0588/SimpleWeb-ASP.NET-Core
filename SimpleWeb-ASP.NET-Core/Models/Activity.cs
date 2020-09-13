5379f11295ccab561b59231bc3ae7ef50e4cdd0a
John0588 <John.Almirol@triosstudent.com>
2020-09-13 17:16:48 -0400

    Added Activity Model, Fixed mistakes and Task Model

--- SimpleWeb-ASP.NET-Core/Models/Activity.cs
+++ SimpleWeb-ASP.NET-Core/Models/Activity.cs
@@ -9,6 +9,31 @@ namespace SimpleWeb_ASP.NET_Core.Models
     {
         // List of Activity Types
         public enum ActivityType // enum data type - stands for enumarations type. Allows to create sets of unchanging list. 
+        {
+            Fitness = 1,
+            Book = 2,
+            Film = 3,
+            TV = 4,
+            VideoGame = 5,
+            Meeting = 6,
+            Work = 7,
+            Education = 8,
+            Walking = 9,
+            Party = 10,
 
+        }
+
+        // Activity Type and Name
+        public Activity(ActivityType activityType, string name = null)
+        {
+            Id = (int)activityType;
+
+            // Use string if name argument unavailable
+            Name = name ?? activityType.ToString(); // ??/??=  - dealing with null values (ex. if the null values is string null values, its Convert To.String vcalues).
+        }
+        // ACITIVITY ID
+        public int Id { get; set; }
+        // ACTIVITY Name
+        public string Name { get; set; }
     }
 }
