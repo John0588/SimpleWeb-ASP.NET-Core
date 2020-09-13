8b657560b92db7a20d4ec557c42c257f3913ab77
John0588 <John.Almirol@triosstudent.com>
2020-09-13 15:03:43 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SimpleWeb-ASP.NET-Core
+* using ASP.NET Core 3.1
+* C#, Razor, HTML, CSS, JavaScript/Blazor
+
+## Description
+Task tracker allows the user to add, list, and delete task, as well as input the amount of time spent of each task.
---
+++ SimpleWeb-ASP.NET-Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleWeb_ASP.NET_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
