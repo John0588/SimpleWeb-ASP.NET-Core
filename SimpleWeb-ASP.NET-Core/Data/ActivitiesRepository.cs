09a85c30e19cccd2b9ef661537a0cbf651281f49
John0588 <John.Almirol@triosstudent.com>
2020-09-13 22:17:43 -0400

    Added Activities data

---
+++ SimpleWeb-ASP.NET-Core/Data/ActivitiesRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimpleWeb_ASP.NET_Core.Models;
+
+namespace SimpleWeb_ASP.NET_Core.Data
+{
+    // Not best practice
+    // Development only
+    public class ActivitiesRepository
+    {
+        public List<Activity> GetActivity()
+        {
+            return Data.Activities
+                .OrderBy(a => a.Name)
+                .ToList();
+        }
+    }
+}
